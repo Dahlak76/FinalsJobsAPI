57a04888afc19be7824b5f88c1a07e65557cad8b
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-09-30 20:10:47 -0500

    shifted margin away from edge/ still rendering fine

--- public/charts.js
+++ public/charts.js
@@ -5,9 +5,25 @@
      data: {
          labels: ['interview', 'declined', 'pending', 'emailed', 'job offered', 'take-home test', 'appointment set', 'applied'],
          datasets: [{
-          label: 'sample',
+          label: 'Current Job Status', 
           data: [1, 2, 3, 4, 2, 4, 6, 1],
-          backgroundColor: 'green'
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
          }]
      },
      options: {},
